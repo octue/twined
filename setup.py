dc5b3f1bd098dd5f3d0d81fa22fada3e4ab26c4b
Tom Clark <>
2019-09-13 13:01:52 +0100

    VER Version bump for the extended documentation

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('LICENSE') as f:
 
 setup(
     name='twined',
-    version='0.0.3',
+    version='0.0.4',
     py_modules=[],
     install_requires=[],
     url='https://www.github.com/octue/twined',
