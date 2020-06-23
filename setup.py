440571efea58cc9d0d15e50fd2c34b340002df1d
Tom Clark <tom@octue.com>
2020-06-23 08:11:41 +0100

    OPS Version bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("LICENSE") as f:
 
 setup(
     name="twined",
-    version="0.0.9",
+    version="0.0.10",
     py_modules=[],
     install_requires=["jsonschema ~= 3.2.0", "python-dotenv"],
     url="https://www.github.com/octue/twined",
