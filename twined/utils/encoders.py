f0983443765828d3bcb6d7a8a77762b71bf46247
Tom Clark <tom@octue.com>
2020-09-17 18:12:03 +0100

    IMP Added new encoders and covered with test cases.

--- twined/utils/encoders.py
+++ twined/utils/encoders.py
@@ -7,9 +7,18 @@ _numpy_spec = importlib.util.find_spec("numpy")
 
 
 class TwinedEncoder(json.JSONEncoder):
-    """ A class which will cope with serialising numpy arrays, ndarrays and matrices in list form.
+    """ An encoder which will cope with serialising numpy arrays, ndarrays and matrices to JSON (in list form.)
 
-    Does not require installation of numpy - it'll only check and serialise if numpy is present.
+    This is designed to work "out of the box" to help people serialise the outputs from twined applications.
+    It does not require installation of numpy - it'll work fine if numpy is not present, so can be used in a versatile
+    tool in uncertain environments.
+
+    Example use:
+    ```
+    from twined.utils import TwinedEncoder
+    some_json = {"a": np.array([0, 1])}
+    json.dumps(some_json, cls=TwinedEncoder)
+    ```
 
     """
 
@@ -17,6 +26,7 @@ class TwinedEncoder(json.JSONEncoder):
         if _numpy_spec is not None:
             import numpy
 
-            if isinstance(obj, numpy.array) or isinstance(obj, numpy.ndarray) or isinstance(obj, numpy.matrix):
+            if isinstance(obj, numpy.ndarray) or isinstance(obj, numpy.matrix):
                 return obj.tolist()
+
         return json.JSONEncoder.default(self, obj)
