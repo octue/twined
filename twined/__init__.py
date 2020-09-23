0bbc2d7db2468e763c77d263e467f4d053fc9ffe
Tom Clark <tom@octue.com>
2020-09-23 10:52:46 +0100

    IMP Export the lists of strands for use by calling libraries

--- twined/__init__.py
+++ twined/__init__.py
@@ -1,2 +1,9 @@
 from . import utils  # noqa: F401
-from .twine import Twine  # noqa: F401
+from .twine import (  # noqa: F401
+    ALL_STRANDS,
+    CHILDREN_STRANDS,
+    CREDENTIAL_STRANDS,
+    MANIFEST_STRANDS,
+    SCHEMA_STRANDS,
+    Twine,
+)
