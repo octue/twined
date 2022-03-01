da174fd63e21e5bdc180a0fd83ef3d8ef1246e0b
cortadocodes <cortado.codes@protonmail.com>
2022-03-01 11:59:53 +0000

    ENH: Raise error if JSON objects contain duplicate keys

--- twined/utils/load_json.py
+++ twined/utils/load_json.py
@@ -33,7 +33,7 @@ def load_json(source, *args, **kwargs):
     if isinstance(source, io.IOBase):
         logger.debug("Detected source is a file-like object, loading contents...")
         check("file-like")
-        return json.load(source, *args, **kwargs)
+        return json.load(source, object_pairs_hook=raise_error_if_duplicate_keys, *args, **kwargs)
 
     elif not isinstance(source, str):
         logger.debug("Source is not a string, bypassing (returning raw data)")
@@ -44,9 +44,27 @@ def load_json(source, *args, **kwargs):
         logger.debug("Detected source is name of a *.json file, loading from %s", source)
         check("filename")
         with open(source) as f:
-            return json.load(f, *args, **kwargs)
+            return json.load(f, object_pairs_hook=raise_error_if_duplicate_keys, *args, **kwargs)
 
     else:
         logger.debug("Detected source is string containing json data, parsing...")
         check("string")
-        return json.loads(source, *args, **kwargs)
+        return json.loads(source, object_pairs_hook=raise_error_if_duplicate_keys, *args, **kwargs)
+
+
+def raise_error_if_duplicate_keys(pairs):
+    """Raise an error if any of the given key-value pairs have the same key.
+
+    :param list(tuple) pairs: a JSON object converted to a list of key-value pairs
+    :raise KeyError: if any of the pairs have the same key
+    :return dict:
+    """
+    result = {}
+
+    for key, value in pairs:
+        if key in result:
+            raise KeyError(f"Duplicate key detected: {key!r}.")
+
+        result[key] = value
+
+    return result
