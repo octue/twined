71a66263ca6ebe25648c813ae17777a38221965a
cortadocodes <cortado.codes@protonmail.com>
2022-03-28 12:15:44 +0100

    ENH: Update manifest schema to only expect dataset paths
    BREAKING CHANGE: This changes the public manifest schema.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("LICENSE") as f:
 
 setup(
     name="twined",
-    version="0.2.2",
+    version="0.3.0",
     py_modules=[],
     install_requires=["jsonschema ~= 4.4.0", "python-dotenv"],
     url="https://www.github.com/octue/twined",
