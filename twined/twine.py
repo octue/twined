d6df9e3f27b210a37e488f18ae8b337cb79aab36
cortadocodes <cortado.codes@protonmail.com>
2022-04-05 12:48:46 +0100

    REF: Simplify Twine.available_manifest_strands property calculation

--- twined/twine.py
+++ twined/twine.py
@@ -49,7 +49,7 @@ class Twine:
             setattr(self, name, strand)
 
         self._available_strands = set(trim_suffix(name, "_schema") for name in vars(self))
-        self._available_manifest_strands = set(set(self._available_strands) & set(MANIFEST_STRANDS))
+        self._available_manifest_strands = self._available_strands & set(MANIFEST_STRANDS)
 
     def _load_twine(self, source=None):
         """Load twine from a *.json filename, file-like or a json string and validates twine contents."""
@@ -214,7 +214,7 @@ class Twine:
     def available_strands(self):
         """Get the names of strands that are found in this twine.
 
-        :return tuple:
+        :return set:
         """
         return self._available_strands
 
@@ -222,7 +222,7 @@ class Twine:
     def available_manifest_strands(self):
         """Get the names of the manifest strands that are found in this twine.
 
-        :return tuple:
+        :return set:
         """
         return self._available_manifest_strands
 
